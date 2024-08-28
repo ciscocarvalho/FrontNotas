7b71ef22032fc6bf4e5e15debaeef69207165fd3
Francisco <francisco.carvalho.q@gmail.com>
2024-08-28 07:39:22 -0300

    Mudando para elastic search em produção

--- src/lib/elasticApi.ts
+++ src/lib/elasticApi.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-
+import { ELASTIC_SEARCH_URL } from "../constants";
 
 export async function createNotas(id:string, titulo: string, conteudo: string|undefined, media: string[], color: string, favorite: boolean) {
   console.log(titulo)
 	try {
-    const response = await axios.post('http://localhost:3000/create', {
+    const response = await axios.post(`${ELASTIC_SEARCH_URL}/create`, {
       id:id,
       titulo: titulo,
       conteudo: conteudo,
@@ -22,7 +22,7 @@ export async function createNotas(id:string, titulo: string, conteudo: string|un
 export async function getNotas(query:string|undefined) {
   
 	try {
-    const response = await axios.get(`http://localhost:3000/documents/${query}`);
+    const response = await axios.get(`${ELASTIC_SEARCH_URL}/documents/${query}`);
     return response
 
   } catch (error) {
@@ -33,7 +33,7 @@ export async function getNotas(query:string|undefined) {
 export async function DeleteNotas(postId:string|undefined) {
   
 	try {
-    const response = await axios.delete(`http://localhost:3000/document/${postId}`);
+    const response = await axios.delete(`${ELASTIC_SEARCH_URL}/document/${postId}`);
     console.log(response)
     return response
 
@@ -45,7 +45,7 @@ export async function DeleteNotas(postId:string|undefined) {
 export async function PutNotas(id:string, titulo?: string, conteudo?: string, media?: string[], color?: string, favorite?: boolean) {
   
 	try {
-    const response = await axios.put(`http://localhost:3000/update/${id}`, {
+    const response = await axios.put(`${ELASTIC_SEARCH_URL}/update/${id}`, {
       id:id,
       titulo: titulo,
       conteudo: conteudo,
